72ed7f15c2580e58513c389835b08e639487461d
daikukai <daikukaibindah@gmail.com>
2025-06-09 16:47:58 +0800

    Implements Main class for application entry point.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        // Ensures that GUI updates are done on the Event Dispatch Thread (EDT)
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+               // new InventoryGUI().setVisible(true);
+            }
+        });
+    }
 }
